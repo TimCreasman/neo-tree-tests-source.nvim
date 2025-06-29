4967a9ddf16578398a0439782c8be2725a081851
Tim Creasman <tcreasman18@gmail.com>
2025-06-29 04:17:33 -0400

    Added remaining deps to mininit.lua

--- scripts/mininit.lua
+++ scripts/mininit.lua
@@ -3,13 +3,16 @@ if #vim.api.nvim_list_uis() == 0 then
     -- Add 'mini.nvim' to 'runtimepath' to be able to use 'mini.test'
     -- Assumed that 'mini.nvim' is stored in 'deps/mini.nvim'
     vim.cmd("set rtp+=deps/mini.nvim")
+    vim.cmd("set rtp+=deps/neo-tree.nvim")
+    vim.cmd("set rtp+=deps/neotest")
+    vim.cmd("set rtp+=deps/nui.nvim")
     vim.cmd("set rtp+=deps/nvim-nio")
+    -- Add 'plenary.nvim' to 'runtimepath' to be able to run tests
+    vim.cmd("set rtp+=deps/plenary.nvim")
 
     -- Set up 'mini.doc'
     require("mini.doc").setup()
 
-    -- Add 'plenary.nvim' to 'runtimepath' to be able to run tests
-    vim.cmd("set rtp+=deps/plenary.nvim")
 
     vim.cmd([[let &rtp.=','.getcwd()]])
 
